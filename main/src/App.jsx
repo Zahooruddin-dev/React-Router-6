a3232209ca05d2768e94727a9c1fee6ab9febeeb
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-29 14:35:57 +0500

    van life

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Home from './Home.jsx'
-import About from './About.jsx';
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx';
 import { 
   BrowserRouter,
   Routes,
@@ -13,14 +13,18 @@ import {
 function App() {
 
   return (
-		<BrowserRouter>
-    <Link to='/'>Home </Link>
-    <Link to='/about'>About </Link>
-    <Routes>
-      <Route path='/' element={<Home></Home>}></Route>
-      <Route path='/about' element={<About></About>}></Route>
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
-		</BrowserRouter>    
+    </BrowserRouter>   
   )
 }
 
