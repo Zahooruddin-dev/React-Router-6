b282d968175959c5b1d333550c4e4ef1e1fbf731
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-29 13:41:40 +0500

    2

--- main/src/About.jsx
+++ main/src/About.jsx
@@ -3,13 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+function about() {
 
   return (
     <>
-    <h1>Home</h1>
+    <h1>About</h1>
     </>
   )
 }
 
-export default App
+export default about
--- main/src/main.jsx
+++ main/src/main.jsx
@@ -1,14 +1,21 @@
-import React, { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from "react-router-dom"
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import About from './About.jsx';
+import './index.css';
+import { 
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-    <App />
-
-    </BrowserRouter>
-  </StrictMode>,
-)
+	<StrictMode>
+		<BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App></App>}></Route>
+      <Route path='/about' element={<About></About>}></Route>
+      </Routes>
+		</BrowserRouter>
+	</StrictMode>
+);
