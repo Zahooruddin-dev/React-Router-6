4ccb5dd4bcdc4c097c4a9bc72b2d1966200e542f
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-30 14:23:16 +0500

    bad

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -11,6 +11,7 @@ import {
   Route,
   Link
 } from 'react-router-dom';
+import { Server } from 'miragejs'
 function App() {
 
   return (
--- main/src/pages/VanList.jsx
+++ main/src/pages/VanList.jsx
@@ -1,10 +1,18 @@
-import React from "react"
-import bgImg from "../assets/images/about-hero.png"
+import React, { useEffect } from "react"
 import { Link } from "react-router-dom"
 
 export default function Vans() {
-    return (
-       <>
-       </>
-    );
+  const [vans, setVans] = React.useState([])
+  useEffect(() => {
+      fetch("/api/vans")
+          .then(res => res.json())
+          .then(data => setVans(data.vans))
+  }, [])
+  
+  
+  console.log(vans);
+  
+  return (
+      <h1>Vans page goes here 🚐</h1>
+  )
 }
