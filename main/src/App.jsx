824f37577316ca2034fff597a3d280b5cafeeac6
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-30 14:03:49 +0500

    1

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './pages/Home.jsx'
+import Vans from './pages/VanList.jsx'
 import About from './pages/About.jsx';
 import { 
   BrowserRouter,
@@ -18,11 +19,13 @@ function App() {
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>   
   )
---
+++ main/src/pages/VanList.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import bgImg from "../assets/images/about-hero.png"
+import { Link } from "react-router-dom"
+
+export default function Vans() {
+    return (
+       <>
+       </>
+    );
+}
