0857bea4406bc5da3e7d1ff803aa84cb436c952f
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-10-01 11:23:58 +0500

    main 4

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -10,9 +10,18 @@ import Layout from './components/Layout.jsx';
 import Dashboard from './pages/Host/Dashboard.jsx';
 import Income from './pages/Host/Income.jsx';
 import Reviews from './pages/Host/Reviews.jsx';
+import HostLayout from './pages/Host/components/Host,.jsx';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './server.js';
 function App() {
+     /**
+
+   * Then replace the parent "/host" route's element below with the
+   * new HostLayout component you made.
+   * 
+   * NOTE: The dashboard route/page will be gone for now, but don't fret.
+   * We'll be fixing that in the next lesson.
+   */
 	return (
 		<BrowserRouter>
 			<Routes>
@@ -21,8 +30,9 @@ function App() {
 					<Route path='/about' element={<About />} />
 					<Route path='/vans' element={<Vans />} />
 					<Route path='/vans/:id' element={<VanDetail />} />
-          <Route path='/host' element={<Dashboard />}>
+          <Route path='/host' element={<HostLayout />}>
 					<Route path='/host/income' element={<Income />} />
+					<Route path='/host/dashboard' element={<Dashboard />} />
 					<Route path='/host/reviews' element={<Reviews />} />
 				</Route>
 				</Route>
