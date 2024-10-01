949f33c9a16405b6833542f20df71ad51b22f194
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-10-01 10:29:07 +0500

    main

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './pages/Home.jsx'
+import VanDetail from './pages/VanDetail.jsx'
 import Vans from './pages/VanList.jsx'
 import About from './pages/About.jsx';
 import { 
@@ -27,6 +28,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>   
   )
