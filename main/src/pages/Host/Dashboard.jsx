36f7efc8391d8d80ac6625cfe53e47ed40a096cf
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-10-01 11:09:25 +0500

    main 3

--- main/src/pages/Host/Dashboard.jsx
+++ main/src/pages/Host/Dashboard.jsx
@@ -1,8 +1,9 @@
 import React from "react"
-
+import { Outlet } from "react-router-dom"
 export default function Dashboard() {
     return (
     <>
+    <Outlet/>
     <h1>Thisn is Dashboard page</h1>
     </>
     )
