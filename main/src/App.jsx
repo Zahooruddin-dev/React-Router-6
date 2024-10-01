c49e45c843fe5e759e4b28c1246a08e926a8675a
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-10-01 10:57:54 +0500

    main2

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -1,31 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from './pages/Home.jsx'
-import VanDetail from './pages/VanDetail.jsx'
-import Vans from './pages/VanList.jsx'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Home from './pages/Home.jsx';
+import VanDetail from './pages/Vans/VanDetail.jsx';
+import Vans from './pages/Vans/VanList.jsx';
 import About from './pages/About.jsx';
-import { 
-  BrowserRouter,
-  Routes,
-  Route,
-  Link
-} from 'react-router-dom';
-import './server.js'
+import Layout from './components/Layout.jsx';
+import Dashboard from './pages/Host/Dashboard.jsx';
+import Income from './pages/Host/Income.jsx';
+import Reviews from './pages/Host/Reviews.jsx';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import './server.js';
 function App() {
-
-  return (
-    <BrowserRouter>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
-      </Routes>
-    </BrowserRouter>   
-  )
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route element={<Layout/>}>
+					<Route path='/' element={<Home />} />
+					<Route path='/about' element={<About />} />
+					<Route path='/host' element={<Dashboard />} />
+					<Route path='/host/income' element={<Income />} />
+					<Route path='/host/reviews' element={<Reviews />} />
+					<Route path='/vans' element={<Vans />} />
+					<Route path='/vans/:id' element={<VanDetail />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;
---
+++ main/src/pages/Host/Reviews.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Reviews() {
+    return (
+    <>
+    <h1>Thisn is Reviews page</h1>
+    </>
+    )
+     
+}
