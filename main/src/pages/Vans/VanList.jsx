4a8e469731531a29dcc634ca9298a60017f02097
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-10-04 14:52:48 +0500

    2

--- main/src/pages/Vans/VanList.jsx
+++ main/src/pages/Vans/VanList.jsx
@@ -33,6 +33,18 @@ export default function Vans() {
 			</Link>
 		</div>
 	));
+  /**
+     * Challenge: add links to filter the vans by type. Use a hard-coded
+     * `to` string like we just practiced. The types are "simple", 
+     * "luxury", and "rugged".
+     * 
+     * For now, give the Links a className of `van-type simple` (and
+     * manually replace "simple" with "luxury" and "rugged" for 
+     * the Links that filter by those types.)
+     * 
+     * Include a Link to clear the filters. Its className should be
+     * `van-type clear-filters`
+     */
 
 	return (
 		<div className='van-list-container'>
