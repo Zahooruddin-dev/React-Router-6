0e614fff5c7395b18582c2695e93d6a97a7a9ed6
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-10-07 15:02:39 +0500

    api in a seprate file

--- main/src/app.js
+++ main/src/app.js
@@ -1,5 +1,5 @@
 export async function getVans() {
-  const res = await fetch ('/api/vans')
-  const data = await data.json()
-  return data.vans
+	const res = await fetch('/api/vans');
+	const data = await data.json();
+	return data.vans;
 }
