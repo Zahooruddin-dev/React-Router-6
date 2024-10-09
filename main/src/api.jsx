63f560474406e090b034b08f5c2b231e0094a65f
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-10-09 17:49:24 +0500

    1

--- main/src/api.jsx
+++ main/src/api.jsx
@@ -1,10 +1,17 @@
-// A function whose only purpose is to delay execution
-// for the specified # of milliseconds when used w/ `await`
-// e.g. inside an async function:
-// await sleep(2000)  => pauses the function for 2 seconds before moving on
-function sleep(ms) {
-	return new Promise((resolve) => setTimeout(() => resolve(), ms));
-}
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore/lite';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyBEjghBbPpZnJujVYq9ENvEVwc8bjf7X0k',
+	authDomain: 'vanlife-a1356.firebaseapp.com',
+	projectId: 'vanlife-a1356',
+	storageBucket: 'vanlife-a1356.appspot.com',
+	messagingSenderId: '825710280807',
+	appId: '1:825710280807:web:1e57dfe9a2d1ebb57394c2',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 export async function getVans(id) {
 	const url = id ? `/api/vans/${id}` : '/api/vans';
@@ -50,21 +57,4 @@ export async function loginUser(creds) {
 	}
 
 	return data;
-}/* 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBEjghBbPpZnJujVYq9ENvEVwc8bjf7X0k",
-  authDomain: "vanlife-a1356.firebaseapp.com",
-  projectId: "vanlife-a1356",
-  storageBucket: "vanlife-a1356.appspot.com",
-  messagingSenderId: "825710280807",
-  appId: "1:825710280807:web:1e57dfe9a2d1ebb57394c2"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig); */
+}
